80a975c1910d6ace0ec2dd731f359bb865db8ddf
Param Bole <pbole@usc.edu>
2020-02-01 19:52:20 -0800

    [Main] Main Table View page

---
+++ DeepCare-iOS/Main/MainTableViewContoller.swift
@@ -0,0 +1,50 @@
+//
+//  MainTableViewContoller.swift
+//  DeepCare-iOS
+//
+//  Created by Param Bole on 2/1/20.
+//  Copyright © 2020 sjkim. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewContoller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension MainTableViewContoller: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+// TODO understand how to return count
+        return 7;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+    
+}
